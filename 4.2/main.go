537a3232b1b96e74704c831a949b8e1da9607acc
罗耀生 <lys1988_cool@126.com>
2019-08-20 11:00:21 +0800

    同步阿里产品列表
    gmt_create 数据格式有问题，后期再处理

--- 4.2/main.go
+++ 4.2/main.go
@@ -12,6 +12,7 @@ func main()  {
 	accessKeyID := "xxxx"
 
 	v := url.Values{}
+	uuidStr,_ := uuid.NewV4()
 	//公共参数
 	v.Add("Format", "JSON")
 	v.Add("Version", "2018-01-20")
@@ -19,7 +20,7 @@ func main()  {
 	v.Add("SignatureMethod", "HMAC-SHA1")
 	v.Add("Timestamp", time.Now().UTC().Format(time.RFC3339))
 	v.Add("SignatureVersion", "1.0")
-	v.Add("SignatureNonce", uuid.NewV4().String())
+	v.Add("SignatureNonce", uuidStr.String())
 	v.Add("RegionId", "cn-shanghai")
 
 	fmt.Println( v )
